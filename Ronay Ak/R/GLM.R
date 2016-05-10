a983d400549139e73c41c31ec002d0c46aa30d14
DanSBS <daniel.samarov@gmail.com>
2016-05-10 11:50:55 -0400

    Update to GLM

--- Ronay Ak/R/GLM.R
+++ Ronay Ak/R/GLM.R
@@ -3,12 +3,12 @@
 # Author: samarov
 ###############################################################################
 library(glmnet)
-GLM <- cv.glmnet(Xm, Y, alpha = 0.5, standardize = FALSE)
+GLM <- cv.glmnet(Xm, Y, alpha = 0.95, standardize = TRUE)
 pY <- predict(GLM, Xm, lambda = 'lambda.min')
 sqrt(mean((Y - pY)^2))
-# [1] 1.801203
+# [1] 1.614917
 
 pYt <- predict(GLM, Xtm, lambda = 'lambda.min')
 sqrt(mean((Yt - pYt)^2))
-# [1] 1.783159
+# [1] 1.515321
 
